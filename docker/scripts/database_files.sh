0988d40a0031ad067f0b5e277b3c095e9c0448d8
Eric Kang <erickangatms@outlook.com>
2018-02-15 16:06:26 -0800

    .

--- docker/scripts/database_files.sh
+++ docker/scripts/database_files.sh
@@ -2,6 +2,6 @@
 wget https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v0.2/WideWorldImporters-Full.bak
 wget https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorksDW2017.bak
 wget https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2017.bak
-wget https://raw.githubusercontent.com/erickangMSFT/sqldevops/master/data_sanitization/sql/restore.sql
+wget https://raw.githubusercontent.com/erickangMSFT/dbprod/master/docker/scripts/wwi_restore.sql
 wget https://raw.githubusercontent.com/erickangMSFT/dbprod/master/docker/scripts/adventureworks_restore.sql
 wget https://raw.githubusercontent.com/erickangMSFT/dbprod/master/docker/scripts/adventureworksDW_restore.sql
